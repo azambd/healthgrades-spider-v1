6dda377a48cfe3a175d1b126ba160c628ef95ad0
Faruque <dev@wscraper.com>
2017-11-28 20:57:11 +0600

    Update not to follow robotstxt

--- healthgrades_spider/settings.py
+++ healthgrades_spider/settings.py
@@ -23,7 +23,7 @@ NEWSPIDER_MODULE = 'healthgrades_spider.spiders'
 #USER_AGENT = 'healthgrades_spider (+http://www.yourdomain.com)'
 
 # Obey robots.txt rules
-ROBOTSTXT_OBEY = True
+ROBOTSTXT_OBEY = False
 
 # Configure maximum concurrent requests performed by Scrapy (default: 16)
 #CONCURRENT_REQUESTS = 32
@@ -31,7 +31,7 @@ ROBOTSTXT_OBEY = True
 # Configure a delay for requests for the same website (default: 0)
 # See http://scrapy.readthedocs.org/en/latest/topics/settings.html#download-delay
 # See also autothrottle settings and docs
-DOWNLOAD_DELAY = 1
+DOWNLOAD_DELAY = 0
 # The download delay setting will honor only one of:
 #CONCURRENT_REQUESTS_PER_DOMAIN = 16
 #CONCURRENT_REQUESTS_PER_IP = 16
